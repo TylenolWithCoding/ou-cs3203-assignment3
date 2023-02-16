11a77a6d2c151340c57e78db6990b5cc9d3ee408
Zack Davis <91405520+TylenolWithCoding@users.noreply.github.com>
2023-02-16 13:55:45 -0600

    Added main, input, and printouts

--- program.py
+++ program.py
@@ -12,3 +12,18 @@ def producter(list):
     
     return sum
 
+
+
+def main():
+    b = []
+    size = int(input("How many numbers are you providing? "))
+
+    for i in range(0, size):
+        c = int(input("Enter your number and press enter after each one: "))
+        b.append(c)
+
+    print(adder(b))
+    print(producter(b))
+if __name__ == "__main__":
+    main()
+
